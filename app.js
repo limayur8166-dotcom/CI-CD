630897e91129fcbe0b456ed1151424e45a241a58
limayur8166-dotcom <limayur8166@gmail.com>
2026-04-14 12:04:44 +0530

    update app to version v2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("🚀 DevOps CI/CD Pipeline Working!");
+  res.send("Version 2 🚀");
 });
 
 app.listen(3000, () => {
